32b7098b5b0375a19e446fa5f78e5eb557ed890d
iHeloa <82479317+iHeloa@users.noreply.github.com>
2022-04-17 10:08:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+video="";
+status1="";
+objects=[];
+function preload(){
+    video=createVideo("video.mp4");
+    video.hide();
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+
+    if(status1 !=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Object detected";
+            document.getElementById("number_of_objects").innerHTML="number of objects detected are:" + objects.length;
+            fill("#ff0000");
+            percent= floor(objects[i].confidence * 100);
+            text(objects[i].label+ " " + percent + "%", objects[i].x + 15,objects[i].y=15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results)
+    objects=results;
+}
+}
+
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Object";
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded");
+    status1=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
